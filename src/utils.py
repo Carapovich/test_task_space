ce3cb5842a1be8ae631cc6aae5151e5a8262865f
Khromenko <nikitahromenko@mail.ru>
2026-01-30 15:20:30 +0300

    Добавил ввод н.у. через csv-таблицу

--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,14 @@
+import csv
+
+def read_initial_conditions(filename) -> dict:
+    with open(filename, mode='r', newline='') as file:
+        ic_dict: dict[str, float] = {}
+        csv_reader = csv.reader(file, delimiter=';')
+        for row in csv_reader:
+            ic_dict.update({ row[0]: float(row[1]) })
+
+    return ic_dict
+
 
 def read_initial_conditions(filename):
     pass
