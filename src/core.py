a06df115e7c5d6f8944b4e5693706d805018f923
Khromenko <nikitahromenko@mail.ru>
2026-02-02 01:11:15 +0300

    Добавлен расчет координат КА относительно координат РН
    Изменен формат представления вектора состояния орбитальных тел (координаты, вектор скорости)

--- src/core.py
+++ src/core.py
@@ -27,12 +27,25 @@ def kepler2eci(a, e, i, long, periapsis, m0) -> NDArray:
     r_o = r_c * np.array([ np.cos(anom_nu), np.sin(anom_nu), 0. ])
     v_o = np.sqrt(EARTH_FM * a) / r_c * np.array([ -np.sin(anom_e), np.sqrt(1 - e**2) * np.cos(anom_e), 0. ])
 
-    # Радиус-вектор, вектор линейной скорости и ускорения тела в ИГЦСК
+    # Радиус-вектор и вектор линейной скорости тела в ИГЦСК
     rot_o2i = R.from_euler('zxz', [periapsis, i, long])
     r_i, v_i = rot_o2i.apply([r_o, v_o])
-    a_i = -EARTH_FM * r_i / r_c**3
 
-    return np.hstack((r_i, v_i, a_i))
+    return np.hstack((r_i, v_i))
+
+def get_state_relative(src_state, distance, yaw, pitch):
+    vec_r = src_state[:3]
+    vec_v = src_state[3:]
+
+    e_x, e_y = vec_v / la.norm(vec_v), -vec_r / la.norm(vec_r)
+    e_z = la.cross(e_x, e_y)
+
+    rot_yaw = R.from_rotvec(-yaw * e_z)
+    rot_pitch = R.from_rotvec(pitch * rot_yaw.apply(e_x))
+    v_bias = (rot_pitch * rot_yaw).apply(e_y) * distance
+
+    return np.hstack((vec_r + v_bias, vec_v))
+
 
 def motion_equation_rhs(t, y_vecs: NDArray):
     vec_r = y_vecs[:3]
